21b56190c565d0a90e51749740d1b64890e18b46
evgeniy.sibagatullin <evgeniy.sibagatullin@gmail.com>
2020-10-24 14:30:39 +0200

    Update movie object

--- src/main/java/com/test/sib/model/Movie.java
+++ src/main/java/com/test/sib/model/Movie.java
@@ -1,10 +1,52 @@
 package com.test.sib.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@SuppressWarnings("unused")
+@Entity
+@Table(name = "movie")
 public class Movie {
 
-    public int id;
-    public String title;
-    public String poster_path;
-    public String overview;
+    @Id
+    private Long id;
 
+    private String title;
+
+    private String poster_path;
+
+    private String overview;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
 }
